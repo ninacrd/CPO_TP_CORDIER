bc2db6fb3bfdfc1084d466c7df259d8decb52794
Nina Cordier <nina.cordier@epfedu.fr>
2022-10-22 22:47:58 +0200

    arrivée à la conversion

---
+++ TP1_convertisseur_CORDIER/src/tp1_convertisseur_cordier/TP1_convertisseur_CORDIER.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_convertisseur_cordier;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Nina
+ */
+public class TP1_convertisseur_CORDIER {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double temperature;
+        double unite;
+        double convertion;
+                
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Saisissez une temperature");
+        temperature = sc.nextDouble();
+        System.out.println("Saisissez l'unite de la temperature donnee");
+        unite = sc.nextDouble();
+        
+        /* question 2 */
+        
+    }
+    
+    public static void CelciusVersKelvin(double tCelcius) {
+        /* à compléter */
+    }
+    
+    public static void KelvinVersCelcius(double tKelvin) {
+        /* à compléter */
+    }
+    
+    public static void FarenheitVersCelcius(double tFarenheit) {
+        /* à compléter */
+    }
+    
+    public static void CelciusVersFarenheit(double tCelcius) {
+        /* à compléter */
+    }
+    
+    public static void KelvinVersFarenheit(double tKelvin) {
+        /* à compléter */
+    }
+    
+    public static void FarenheitVersKelvin(double tKFarenheit) {
+        /* à compléter */
+    }
+}
+        /*double Celcius;
+        double Kelvin;
+        double Farenheit;*/
